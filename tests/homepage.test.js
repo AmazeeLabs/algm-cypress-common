d725b0c98d965a8c95fb70628267e866a2a2f5fc
eli.stone <eli.stone@amazee.com>
2021-06-09 14:32:53 +0100

    reworking the homepage tests to have better settings and information

--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -1,7 +1,21 @@
-it('loads login page', () => {
-  cy.visit('/user/login');
-})
+describe('Homepage validation', () => {
+  const envSettings = Cypress.env('homepage');
 
-it('can login', () => {
-  cy.drupalLogin('admin', 'admin123');
-})
+  before(() => {
+    cy.skipOn(envSettings.enabled !== true);
+    cy.visit('/');
+  });
+
+  it('it has only one h1 tag', () => {
+    cy.get('h1').should('have.length', 1).and('not.be.empty');
+  });
+
+  it('has skip to main content link', () => {
+    cy.contains('Skip to main content').should('have.class', 'visually-hidden');
+  });
+
+  it('it has cookie banner', () => {
+    const cookieSettings = envSettings['cookie'];
+    cy.get(cookieSettings['class']).should('be.visible');
+  });
+});
