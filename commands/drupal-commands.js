f25484d8898d71f6b5af54b42adfa8d6e1d0db42
eli.stone <eli.stone@amazee.com>
2021-06-16 14:57:05 +0100

    :wrench: added custom command to allow skipping any test

--- commands/drupal-commands.js
+++ commands/drupal-commands.js
@@ -17,16 +17,21 @@ Cypress.Commands.add("drupalLogout", () => {
 
 Cypress.Commands.add("drupalDrushCommand", (command) => {
   var cmd = Cypress.env("drupalDrushCmdLine");
-
   if (cmd === null) {
     cmd = "drush %command";
   }
-
-  if( typeof command === "string" ) {
-    command = [ command ];
+  if (typeof command === "string") {
+    command = [command];
   }
-
   const execCmd = cmd.replace("%command", command.join(" "));
-
   return cy.exec(execCmd);
+});
+
+Cypress.Commands.add("checkIfSkipped", (id) => {
+  let found = false;
+  const skippedIds = Cypress.env('skipped_tests');
+  if (Array.isArray(skippedIds)) {
+    found = skippedIds.includes(id);
+  }
+  return found;
 });
--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -7,14 +7,17 @@ describe('Homepage validation', () => {
   });
 
   it('it has only one h1 tag', () => {
+    cy.checkIfSkipped('homepage.one_h1').then(status => cy.skipOn(status));
     cy.get('h1').should('have.length', 1).and('not.be.empty');
   });
 
   it('has skip to main content link', () => {
+    cy.checkIfSkipped('homepage.main_content_link').then(status => cy.skipOn(status));
     cy.contains('Skip to main content').should('have.class', 'visually-hidden');
   });
 
-  it('it has cookie banner', () => {
+  it('has cookie banner displayed', () => {
+    cy.checkIfSkipped('homepage.cookie_banner').then(status => cy.skipOn(status));
     const cookieSettings = envSettings['cookie'];
     cy.get(cookieSettings['class']).should('be.visible');
   });
